d8b31cb60ff65e94804e12977259f61bac8d6e69
dpecsi <pecsi.daniel@students.jedlik.eu>
2026-02-05 17:34:25 +0100

    feladat

--- 20260205 - CSharp alapok/Program.cs
+++ 20260205 - CSharp alapok/Program.cs
@@ -4,33 +4,49 @@
     {
         static void Main(string[] args)
         {
-            int szám;
-            szám = 5;
+            //int szám;
+            //szám = 5;
 
-            int szám2 = -18;
+            //int szám2 = -18;
 
-            const double PI = 3.14159;
+            //const double PI = 3.14159;
 
-            szám = szám2 + 10;
-            szám = szám2 - 10;
-            szám = szám2 * 10;
-            szám = szám2 / 10;  // egész osztás
-            double eredmény = PI / 18.0; // valós osztás
-            // valami megjegyzés
-            /*
-             több soros megjegyzés
-             több soros megjegyzés
-             */
-            szám = szám2 % 10; // maradákos osztás
-            szám2 = 180;
-            eredmény = szám2 / 18.0; // valós osztás
-            szám = Convert.ToInt32(szám2 / 18.0); // valós osztás eeedményét egészre konvertáljuk
-            eredmény = szám2 / 18; // valós osztás, mert a szám2 egy int, de a 18 egy int literal
+            //szám = szám2 + 10;
+            //szám = szám2 - 10;
+            //szám = szám2 * 10;
+            //szám = szám2 / 10;  // egész osztás
+            //double eredmény = PI / 18.0; // valós osztás
+            //// valami megjegyzés
+            ///*
+            // több soros megjegyzés
+            // több soros megjegyzés
+            // */
+            //szám = szám2 % 10; // maradákos osztás
+            //szám2 = 180;
+            //eredmény = szám2 / 18.0; // valós osztás
+            //szám = Convert.ToInt32(szám2 / 18.0); // valós osztás eeedményét egészre konvertáljuk
+            //eredmény = szám2 / 18; // valós osztás, mert a szám2 egy int, de a 18 egy int literal
 
-            Console.Write(szám);
-            Console.Write(eredmény);
-            Console.WriteLine();
-            Console.WriteLine("Hello, World!");
+            //Console.Write(szám);
+            //Console.Write(eredmény);
+            //Console.WriteLine();
+            //Console.WriteLine("Hello, World!");
+
+            int egészSzám = 0;
+            do
+            {
+                Console.Write("Írj be egy egész számot 10 és 20 között: ");
+                string adat = Console.ReadLine();
+                try
+                {
+                    egészSzám = Convert.ToInt32(adat);
+                    if (egészSzám < 10) Console.WriteLine("Túl kicsi számot írtál be, próbáld újra.");
+                    if (egészSzám > 20) Console.WriteLine("Túl nagy számot írtál be, próbáld újra.");
+                }
+                catch {
+                    Console.WriteLine("Nem sikerült a konvertálás, rossz számot írtál be.");
+                }
+            }while (egészSzám < 10 || egészSzám > 20);
         }
     }
 }
